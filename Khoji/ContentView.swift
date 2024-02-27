8b908b0926322fcd9ccf931eb306d0b651b56cf0
fasaMaps <fasa@mapspeople.com>
2024-02-27 02:29:05 +0100

    basic search

--- Khoji/ContentView.swift
+++ Khoji/ContentView.swift
@@ -2,10 +2,7 @@ import SwiftUI
 import Combine
 
 struct ContentView: View {
-    @State private var searchText = ""
     @ObservedObject var sharedState: SharedState
-    
-    private let dummyResults = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]
 
     var body: some View {
         VStack {
@@ -30,9 +27,8 @@ struct ContentView: View {
             .background(Color(.systemGray))
             .cornerRadius(UIConstants.searchBarCornerRadius)
             
-            // List appears when user starts typing
             if !sharedState.searchText.isEmpty {
-                List(dummyResults, id: \.self) { item in
+                List(sharedState.searchResults, id: \.self) { item in
                     Text(item)
                 }
                 .frame(maxHeight: 200)
@@ -41,8 +37,66 @@ struct ContentView: View {
     }
 }
 
+// Also a Search ViewModel
 class SharedState: ObservableObject {
     @Published var searchText: String = ""
+    @Published var searchResults: [String] = []
+
+    private var query: NSMetadataQuery?
+    private var cancellables = Set<AnyCancellable>()
+
+    init() {
+        $searchText
+            .debounce(for: .milliseconds(500), scheduler: RunLoop.main)
+            .removeDuplicates()
+            .sink { [weak self] newText in
+                self?.performSearch(query: newText)
+            }
+            .store(in: &cancellables)
+    }
+
+    private func performSearch(query searchText: String) {
+        guard !searchText.trimmingCharacters(in: .whitespaces).isEmpty else {
+            self.searchResults = []
+            return
+        }
+
+        // Stop previous query if any
+        query?.stop()
+        NotificationCenter.default.removeObserver(self, name: .NSMetadataQueryDidFinishGathering, object: nil)
+
+        // Initialize query
+        let metadataQuery = NSMetadataQuery()
+        metadataQuery.predicate = NSPredicate(format: "%K CONTAINS[cd] %@", NSMetadataItemFSNameKey, searchText)
+        metadataQuery.searchScopes = [NSMetadataQueryUserHomeScope, NSMetadataQueryLocalComputerScope]
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(queryDidFinishGathering(_:)),
+            name: .NSMetadataQueryDidFinishGathering,
+            object: metadataQuery)
+
+        self.query = metadataQuery
+        metadataQuery.start()
+    }
+
+    @objc private func queryDidFinishGathering(_ notification: Notification) {
+        guard let query = notification.object as? NSMetadataQuery else { return }
+        query.stop() // Important to stop the query to free up resources
+
+        var results: [String] = []
+        for item in query.results as! [NSMetadataItem] {
+            if let path = item.value(forAttribute: NSMetadataItemPathKey) as? String {
+                results.append(path)
+            }
+        }
+
+        DispatchQueue.main.async {
+            self.searchResults = results
+        }
+
+        NotificationCenter.default.removeObserver(self, name: .NSMetadataQueryDidFinishGathering, object: query)
+    }
 }
 
 /*#Preview {
